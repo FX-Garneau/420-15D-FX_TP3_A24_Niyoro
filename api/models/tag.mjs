7d7e0ef4d54b8c8ba57f9fe09dc5b08d1bc2f32b
Ulric Huot <ulric.huot@gmail.com>
2024-11-22 15:20:42 -0500

    ✍️ Completed tag.mjs

--- api/models/tag.mjs
+++ api/models/tag.mjs
@@ -1,8 +1,11 @@
 import { Schema, model } from "mongoose";
 
 const tagSchema = new Schema({
-
+   name: {
+      type: String,
+      maxLength: [50, "Le `name` ne doit pas dépasser 50 caractères"],
+      required: [true, "Le champ `name`est requis"]
+   }
 }, { timestamps: true });
-
 
 export default model("Tag", tagSchema);
