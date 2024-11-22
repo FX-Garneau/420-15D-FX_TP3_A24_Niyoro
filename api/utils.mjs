97adf9ce4ea196bcd4bac5ea0598a22e323c8f4e
Ulric Huot <ulric.huot@gmail.com>
2024-11-22 16:16:44 -0500

    ⚠️ Added ResponseError class

--- api/utils.mjs
+++ api/utils.mjs
@@ -22,3 +22,27 @@ export function getValidationErrorMessage(err) {
          .join("\n")
       : null;
 }
+
+/**
+ * Classe personnalisée pour les erreurs de réponse
+ * @class
+ */
+export class ResponseError extends Error {
+   constructor(statusCode = 500, message, data) {
+      super(message);
+      this.statusCode = statusCode;
+      this.data = data;
+   }
+
+   /**
+    * Convertit l'erreur en objet JSON pour l'envoyer au client
+    * @method
+    * @returns {object} L'objet JSON représentant l'erreur
+    */
+   toJSON() {
+      return {
+         message: this.message,
+         data: this.data
+      };
+   }
+}
