8cc9cda303cf3c30d2bbf384f485ce711cd67e09
Ulric Huot <ulric.huot@gmail.com>
2024-11-26 12:02:51 -0500

    🌱 Add seeds for items

--- api/seeds/items.mjs
+++ api/seeds/items.mjs
@@ -1,1 +1,188 @@
-// TODO/seed: Add at least 3 items per user, including private, public, and pinned items
+// DONE: Add at least 3 items per user, including private, public, and pinned items
+export default [
+   // User 1 (650dade494237ad07eedc7ee)
+   {
+      _id: "650dcaf47bc86e471e5c0afc",
+      title: "Item public",
+      content: "Ceci est un item public",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: false,
+      created_by: "650dade494237ad07eedc7ee",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0af9"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0afd",
+      title: "Item privé",
+      content: "Ceci est un item privé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: true,
+      sticky: false,
+      created_by: "650dade494237ad07eedc7ee",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afa"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0afe",
+      title: "Item épinglé",
+      content: "Ceci est un item épinglé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: true,
+      created_by: "650dade494237ad07eedc7ee",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afb"]
+   },
+   // User 2 (650dadf594237ad07eedc7f0)
+   {
+      _id: "650dcaf47bc86e471e5c0aff",
+      title: "Item public",
+      content: "Ceci est un item public",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: false,
+      created_by: "650dadf594237ad07eedc7f0",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0af9"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b00",
+      title: "Item privé",
+      content: "Ceci est un item privé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: true,
+      sticky: false,
+      created_by: "650dadf594237ad07eedc7f0",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afa"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b01",
+      title: "Item épinglé",
+      content: "Ceci est un item épinglé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: true,
+      created_by: "650dadf594237ad07eedc7f0",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afb"]
+   },
+   // User 3 (650dcaf47bc86e471e5c0af6)
+   {
+      _id: "650dcaf47bc86e471e5c0b02",
+      title: "Item public",
+      content: "Ceci est un item public",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af6",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0af9"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b03",
+      title: "Item privé",
+      content: "Ceci est un item privé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: true,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af6",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afa"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b04",
+      title: "Item épinglé",
+      content: "Ceci est un item épinglé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: true,
+      created_by: "650dcaf47bc86e471e5c0af6",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afb"]
+   },
+   // User 4 (650dcaf47bc86e471e5c0af7)
+   {
+      _id: "650dcaf47bc86e471e5c0b05",
+      title: "Item public",
+      content: "Ceci est un item public",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af7",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0af9"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b06",
+      title: "Item privé",
+      content: "Ceci est un item privé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: true,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af7",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afa"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b07",
+      title: "Item épinglé",
+      content: "Ceci est un item épinglé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: true,
+      created_by: "650dcaf47bc86e471e5c0af7",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afb"]
+   },
+   // User 5 (650dcaf47bc86e471e5c0af8)
+   {
+      _id: "650dcaf47bc86e471e5c0b08",
+      title: "Item public",
+      content: "Ceci est un item public",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af8",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0af9"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b09",
+      title: "Item privé",
+      content: "Ceci est un item privé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: true,
+      sticky: false,
+      created_by: "650dcaf47bc86e471e5c0af8",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afa"]
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b0a",
+      title: "Item épinglé",
+      content: "Ceci est un item épinglé",
+      latitude: 45.5017,
+      longitude: -73.5673,
+      private: false,
+      sticky: true,
+      created_by: "650dcaf47bc86e471e5c0af8",
+      permalink: null,
+      tags: ["650dcaf47bc86e471e5c0afb"]
+   }
+]
