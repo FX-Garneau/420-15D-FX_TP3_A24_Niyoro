dee703503d240a0464e727b9e7f6a2bf8b60626b
Ulric Huot <ulric.huot@gmail.com>
2024-11-26 12:03:45 -0500

    🌱 Add seeds for reactions

--- api/seeds/reactions.mjs
+++ api/seeds/reactions.mjs
@@ -1,1 +1,33 @@
-// TODO/seed: Add reactions for multiple items
+// DONE: Add reactions for multiple items
+export default [
+   {
+      _id: "650dcaf47bc86e471e5c0b0b",
+      type: "J'aime",
+      user_id: "650dadf594237ad07eedc7f0",
+      item_id: "650dade494237ad07eedc7ee"
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b0c",
+      type: "J'aime",
+      user_id: "650dcaf47bc86e471e5c0af7",
+      item_id: "650dadf594237ad07eedc7f0"
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b0d",
+      type: "J'aime",
+      user_id: "650dcaf47bc86e471e5c0af8",
+      item_id: "650dcaf47bc86e471e5c0afd"
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b0e",
+      type: "J'aime",
+      user_id: "650dcaf47bc86e471e5c0af8",
+      item_id: "650dcaf47bc86e471e5c0afe"
+   },
+   {
+      _id: "650dcaf47bc86e471e5c0b0f",
+      type: "J'aime",
+      user_id: "650dcaf47bc86e471e5c0af8",
+      item_id: "650dcaf47bc86e471e5c0b00"
+   }
+];
