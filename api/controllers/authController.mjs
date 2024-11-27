873d9d6f9a3bfedd8b33007cc1e65a7f7db9eb03
Ulric Huot <ulric.huot@gmail.com>
2024-11-27 12:10:43 -0500

    ✨ Implement login function with validation, user lookup, password verification, and token generation

--- api/controllers/authController.mjs
+++ api/controllers/authController.mjs
@@ -1,12 +1,30 @@
+import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../models/user.mjs";
+import { User } from "../models/user.mjs";
+import { ResponseError } from "../utils.mjs";
 
-// TODO/JsDoc: Document this function
+/**
+ * Connecte un utilisateur
+ * @param {express.Request<any, any, { email: string, password: string }>} req 
+ * @param {express.Response} res 
+ * @param {express.NextFunction} next 
+ */
 export async function login(req, res, next) {
    const email = req.body?.email?.toString()?.trim()?.toLowerCase() ?? "";
    const password = req.body?.password?.toString()?.trim() ?? "";
-   // TODO: Implement login function
+   // Validation des données
+   if (!email) throw new ResponseError(400, "Le champ 'email' est requis.");
+   if (!password) throw new ResponseError(400, "Le champ 'password' est requis.");
+   // Recherche de l'utilisateur
+   const user = await User.findOne({ email });
+   if (!user) throw new ResponseError(404, "Utilisateur non trouvé.");
+   // Vérification du mot de passe
+   if (await bcrypt.compare(password, user.password)) throw new ResponseError(401, "Mot de passe incorrect.");
+   // Génération du token
+   const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: "24h" });
+   // Envoi de la réponse
+   res.json({ token });
 }
 
 // TODO/JsDoc: Document this function
