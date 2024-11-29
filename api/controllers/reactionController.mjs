d6cdcf48cf205b5d9187d693d0626e277288fc84
Ulric Huot <ulric.huot@gmail.com>
2024-11-29 11:35:11 -0500

    📦 Completed reaction CRUD controller

--- api/controllers/reactionController.mjs
+++ api/controllers/reactionController.mjs
@@ -1,19 +1,38 @@
+import express from "express";
 import { Reaction } from "../models/reaction.mjs";
 
-// Récupère toutes les réactions associées à un item spécifique
-// TODO/JsDoc: Document this function
+/**
+ * Récupère toutes les réactions associées à un item spécifique
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 export async function getReactionsByItem(req, res, next) {
-   // TODO: Implement getReactionsByItem function
+   req.user && req.resource instanceof Reaction
+      ? res.json(req.resource)
+      : next(new Error);
 }
 
-// Crée une nouvelle réaction
-// TODO/JsDoc: Document this function
+/**
+ * Crée une nouvelle réaction pour un item spécifique
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 export async function createReaction(req, res, next) {
-   // TODO: Implement createReaction function
+   req.user && req.resource instanceof Reaction
+      ? Reaction.create(req.body).then(res.status(201).json, next)
+      : next(new Error);
 }
 
-//  Supprime une réaction existante par son identifiant
-// TODO/JsDoc: Document this function
+/**
+ * Supprime une réaction existante par son identifiant
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 export async function deleteReaction(req, res, next) {
-   // TODO: Implement deleteReaction function
+   req.user && req.resource instanceof Reaction
+      ? req.resource.deleteOne().then(res.json, next)
+      : next(new Error);
 }
--- api/routes/reaction.mjs
+++ api/routes/reaction.mjs
@@ -1,16 +1,19 @@
 import express from "express";
 import { createReaction, getReactionsByItem, deleteReaction } from "../controllers/reactionController.mjs";
 import { isAuth } from "../middleware/authentication.mjs";
+import { prefetch } from "../middleware/resource.mjs";
+import { Item } from "../models/item.mjs";
+import { Reaction } from "../models/reaction.mjs";
 
 const router = express.Router();
 
 // Crée une nouvelle réaction pour un item spécifique
-router.post("/items/:item_id/reactions", isAuth(true), createReaction);
+router.post("/items/:item_id/reactions", isAuth(true), prefetch(Item, "item_id"), createReaction);
 
 // Récupère toutes les réactions pour un item spécifique
-router.get("/items/:item_id/reactions", isAuth(true), getReactionsByItem);
+router.get("/items/:item_id/reactions", isAuth(true), prefetch(Item, "item_id"), getReactionsByItem);
 
 // Supprime une réaction spécifique par son identifiant
-router.delete("/reactions/:reaction_id", isAuth(true), deleteReaction);
+router.delete("/reactions/:reaction_id", isAuth(true), prefetch(Reaction, "reaction_id"), deleteReaction);
 
 export default router;
