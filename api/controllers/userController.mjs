33c2948f9d812591672f8b8f4f8b6bb34b6228de
Ulric Huot <ulric.huot@gmail.com>
2024-11-28 20:25:52 -0500

    📦 Completed user CRUD controller

--- api/controllers/userController.mjs
+++ api/controllers/userController.mjs
@@ -1,98 +1,55 @@
 import express from "express";
-import mongoose from "mongoose";
 import { User } from "../models/user.mjs";
-import { Item } from "../models/item.mjs";
-import { Reaction } from "../models/reaction.mjs";
 import { ResponseError } from "../utils.mjs";
 
-// Récupère les utilisateurs
-// TODO/JsDoc: Document this function
+/**
+ * Récupère la liste des utilisateurs
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
+ */
 export async function getUsers(req, res, next) {
-   // TODO: Implement getUsers function
+   User.find().then(res.json, next);
 }
 
 /**
- * Obtienir les informations de l'utilisateur connecté ou d'un utilisateur spécifique par ID
- * @param {express.Request<{id?:string}>} req 
- * @param {express.Response} res 
- * @param {express.NextFunction} next 
+ * Obtient les informations de l'utilisateur connecté ou d'un utilisateur spécifique par ID
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
  */
 export async function getUser(req, res, next) {
-   if (req.params.id) {
-      // Valider l'ID de l'utilisateur
-      if (!mongoose.isValidObjectId(req.params.id)) return next(new ResponseError(400));
-      // Obtenir les informations de l'utilisateur par ID
-      else User.findById(req.params.id)
-         .then(user => {
-            if (user) res.json(user.toJSON());
-            else next(new ResponseError(404, "L'utilisateur n'existe pas"));
-         }).catch(next);
-   } else {
-      // Obtenir les informations de l'utilisateur connecté
-      if (req.user) res.json(req.user.toJSON());
-      else next(new ResponseError(401));
-   }
+   req.resource instanceof User
+      ? res.json(req.resource)
+      : next(new ResponseError(404, "L'utilisateur n'existe pas"));
 }
 
-// 
 /**
- * Mettre à jour les informations de l'utilisateur connecté ou d'un utilisateur spécifique par ID
- * @param {express.Request<{id?:string}>} req 
- * @param {express.Response} res 
- * @param {express.NextFunction} next 
+ * Met à jour les informations de l'utilisateur connecté ou d'un utilisateur spécifique par ID
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
  */
 export async function updateUser(req, res, next) {
-   // Filtrer les options modifiables
-   const options = {
-      first_name: req.body.firstName,
-      last_name: req.body.lastName,
-      email: req.body.email,
-      password: req.body.password,
-      avatar: req.body.avatar,
-   };
-
-   if (req.params.id) {
-      // Valider l'ID de l'utilisateur
-      if (!mongoose.isValidObjectId(req.params.id)) return next(new ResponseError(400));
-      // Mettre à jour les informations de l'utilisateur par ID
-      else User.findByIdAndUpdate(req.params.id, options, { new: true })
-         .then(user => {
-            if (user) res.json(user.toJSON());
-            else next(new ResponseError(404, "L'utilisateur n'existe pas"));
-         }).catch(next);
-   } else {
-      // Mettre à jour les informations de l'utilisateur connecté
-      if (req.user) {
-         // Mettre à jour les informations de l'utilisateur
-         Object.assign(req.user, options);
-         req.user.save()
-            .then(user => res.status(204).end())
-            .catch(next);
-      } else next(new ResponseError(401));
-   }
+   req.resource instanceof User && req.user
+      ? req.resource.updateOne({
+         first_name: req.body.firstName,
+         last_name: req.body.lastName,
+         email: req.body.email,
+         password: req.body.password,
+         avatar: req.body.avatar,
+      }, { new: true }).then(res.json, next)
+      : next(new ResponseError(404, "L'utilisateur n'existe pas"));
 }
 
-// Supprimer un utilisateur par ID
 /**
- * Supprimer l'utilisateur connecté ou un utilisateur spécifique par ID
+ * Supprime l'utilisateur connecté ou un utilisateur spécifique par ID
+ * @param {express.Request} req
+ * @param {express.Response} res
+ * @param {express.NextFunction} next
  */
 export async function deleteUser(req, res, next) {
-   if (req.params.id) {
-      // Valider l'ID de l'utilisateur
-      if (!mongoose.isValidObjectId(req.params.id)) return next(new ResponseError(400));
-      // Supprimer l'utilisateur par ID
-      else User.findByIdAndDelete(req.params.id)
-         .then(user => {
-            if (user) res.status(204).end();
-            else next(new ResponseError(404, "L'utilisateur n'existe pas"));
-         }).catch(next);
-   } else {
-      // Supprimer l'utilisateur connecté
-      if (req.user) {
-         // Supprimer l'utilisateur connecté
-         req.user.delete()
-            .then(() => res.status(204).end())
-            .catch(next);
-      } else next(new ResponseError(401));
-   }
+   req.resource instanceof User
+      ? req.resource.deleteOne().then(res.json, next)
+      : next(new ResponseError(404, "L'utilisateur n'existe pas"));
 }
--- api/routes/user.mjs
+++ api/routes/user.mjs
@@ -1,24 +1,26 @@
 import { Router } from "express";
 import { getUser, getUsers, updateUser, deleteUser } from "../controllers/userController.mjs";
 import { isAdmin, isAuth } from "../middleware/authentication.mjs";
+import { prefetch } from "../middleware/resource.mjs";
+import { User } from "../models/user.mjs";
 
 const router = Router();
 
 // Routes pour l'utilisateur connecté
 // Obtenir les informations de l'utilisateur connecté
-router.get("/me", isAuth(true), getUser);
+router.get("/me", isAuth(true), prefetch(User, r => r.user?.id), getUser);
 // Mettre à jour les informations de l'utilisateur connecté
-router.put("/me", isAuth(true), updateUser);
+router.put("/me", isAuth(true), prefetch(User, r => r.user?.id), updateUser);
 // Supprimer le compte de l'utilisateur connecté
-router.delete("/me", isAuth(true), deleteUser);
+router.delete("/me", isAuth(true), prefetch(User, r => r.user?.id), deleteUser);
 
 // Routes pour l'accès par ID (ex : admin ou vue de profil)
 // Obtenir les informations d'un utilisateur spécifique
-router.get("/users/:id", isAuth(true), getUser);
+router.get("/users/:id", isAuth(true), prefetch(User, "id"), getUser);
 // Mise à jour d'un utilisateur spécifique (réservée aux administrateurs)
-router.put("/users/:id", isAuth(true), isAdmin, updateUser);
+router.put("/users/:id", isAuth(true), isAdmin, prefetch(User, "id"), updateUser);
 // Suppression d'un utilisateur spécifique (réservée aux administrateurs)
-router.delete("/users/:id", isAuth(true), isAdmin, deleteUser);
+router.delete("/users/:id", isAuth(true), isAdmin, prefetch(User, "id"), deleteUser);
 
 // Route pour lister tous les utilisateurs
 router.get("/users", isAuth(true), getUsers);
