81141f934a6a28e6ecab921fc125d61a285c9524
Ulric Huot <ulric.huot@gmail.com>
2024-12-04 14:24:51 -0500

    Dracula theme + Formatting

--- site/src/App.vue
+++ site/src/App.vue
@@ -7,13 +7,13 @@ import HelloWorld from './components/HelloWorld.vue'
    <header>
       <div class="navbar bg-base-100">
          <div class="flex-none">
-            <button class="btn btn-square btn-ghost">
-               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                  class="inline-block h-5 w-5 stroke-current">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
-                  </path>
-               </svg>
-            </button>
+            <details class="dropdown">
+               <summary class="btn m-1">open or close</summary>
+               <ul class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+                  <li><a>Item 1</a></li>
+                  <li><a>Item 2</a></li>
+               </ul>
+            </details>
          </div>
          <div class="flex-1">
             <a class="btn btn-ghost text-xl">daisyUI</a>
