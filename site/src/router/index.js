fb552ea7957b6978945227f70d190102e39aeb2e
Ulric Huot <ulric.huot@gmail.com>
2024-12-04 14:46:11 -0500

    🛣️ Added new routes for home, signup, login, tag, item, add, tags, and profile in the router

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -3,28 +3,43 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
    history: createWebHistory(import.meta.env.BASE_URL),
    routes: [
-      // {
-      //    path: '/',
-      //    name: 'home',
-      //    component: HomeView,
-      // },
-      // {
-      //    path: '/about',
-      //    name: 'about',
-      //    // route level code-splitting
-      //    // this generates a separate chunk (About.[hash].js) for this route
-      //    // which is lazy-loaded when the route is visited.
-      //    component: () => import('../views/AboutView.vue'),
-      // },
+      {
+         path: '/',
+         name: 'home',
+      },
+      {
+         path: '/signup',
+         name: 'signup',
+      },
+      {
+         path: '/login',
+         name: 'login',
+      },
+      {
+         path: '/:tag',
+         name: 'tag',
+      },
+      {
+         path: '/item/:id',
+         name: 'item',
+      },
+      {
+         path: '/add',
+         name: 'add',
+      },
+      {
+         path: '/tags',
+         name: 'tags',
+      },
+      {
+         path: '/profile/:id?',
+         name: 'profile',
+      }
    ],
 })
 
 router.beforeEach((to, from, next) => {
-   // if (to.name !== 'login' && !isAuthenticated) {
-   //    next({ name: 'login' })
-   // } else {
-   //    next()
-   // }
+   // TODO: Make /tags route only available if connected user is admin
 })
 
 export default router
