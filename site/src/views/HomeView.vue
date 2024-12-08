361a336ddd1c68bcbeca2500b942ef2ea2031a5f
Ulric Huot <ulric.huot@gmail.com>
2024-12-08 04:13:04 -0500

    ✨ Update HomeView component to integrate tags store and improve item filtering logic

--- site/src/views/HomeView.vue
+++ site/src/views/HomeView.vue
@@ -1,61 +1,69 @@
 <script setup>
-import FormComponent from '@/components/FormComponent.vue';
-import ItemCard from '@/components/ItemCard.vue';
-import { useItemsStore } from '@/stores/items';
-import { useUserStore } from '@/stores/user';
-import { storeToRefs } from 'pinia';
-import { computed, ref, watchEffect } from 'vue';
+import FormComponent from '@/components/FormComponent.vue'
+import ItemCard from '@/components/ItemCard.vue'
+import { useItemsStore } from '@/stores/items'
+import { useTagsStore } from '@/stores/tags'
+import { useUserStore } from '@/stores/user'
+import { storeToRefs } from 'pinia'
+import { computed, ref } from 'vue'
 
 // Initier l'état du filtre "Montrer seulement mes items" à faux
 const onlyMine = ref(false);
 
 // Récupérer les stores
-const userStore = useUserStore();
-const itemsStore = useItemsStore();
-const { items } = storeToRefs(itemsStore);
+const userStore = useUserStore()
+const tagsStore = useTagsStore()
+const itemsStore = useItemsStore()
+const { items } = storeToRefs(itemsStore)
 
 // Filtrer les items visibles
 const visibleItems = computed(() =>
-   onlyMine.value === true
-      ? items.value.filter(item => item.created_by === userStore.account?.id)
+   onlyMine.value && userStore.account != null
+      ? items.value.filter(item => item.created_by === userStore.account?._id)
       : items.value.filter(item => item.private === false)
 )
 
-// Récupérer les items
-const loadingItems = ref(false);
-async function refreshItems() {
+// Récupérer les items et les tags
+const refreshing = ref(false)
+async function refresh() {
    try {
-      loadingItems.value = true;
-      await itemsStore.syncGlobalPublicItems();
+      refreshing.value = true
+      await tagsStore.syncTags()
+      await itemsStore.syncGlobalPublicItems()
       if (userStore.isAuthenticated)
-         await itemsStore.syncMyItems();
+         await itemsStore.syncMyItems()
    } catch {
       // Ignorer les erreurs
    } finally {
       setTimeout(() => {
-         loadingItems.value = false;
+         refreshing.value = false
       }, 100);
    }
 }
-refreshItems();
+refresh()
 </script>
 
 <template>
-   <div class="container flex flex-col gap-8 w-full">
+   <div class="container flex flex-col gap-8 w-full p-4">
       <h1 class="text-3xl">Accueil</h1>
       <div>
          <h2 class="text-xl mb-2">Filtres</h2>
-         <FormComponent kind="checkbox" label="Montrer seulement mes items" v-model="onlyMine" />
+         <template v-if="userStore.account">
+            <FormComponent kind="checkbox" label="Montrer seulement mes items" v-model="onlyMine" />
+         </template>
+         <div v-else class="tooltip tooltip-bottom" data-tip="Vous devez être connecter pour utiliser cette option">
+            <FormComponent kind="checkbox" label="Montrer seulement mes items" v-model="onlyMine" disabled />
+         </div>
       </div>
       <div>
          <h2 class="text-xl mb-2 flex items-center gap-2">
             Items
-            <button @click="refreshItems" class="btn btn-xs btn-square btn-accent btn-outline">
-               <span v-if="loadingItems" class="loading loading-spinner loading-xs"></span>
+            <button @click="refresh" class="btn btn-xs btn-square btn-accent btn-outline">
+               <span v-if="refreshing" class="loading loading-spinner loading-xs"></span>
                <i v-else class="bi bi-arrow-clockwise"></i>
             </button>
          </h2>
-         <div class="flex flex-col gap-4">
+         <div class="flex flex-wrap gap-4">
             <ItemCard v-for="item in visibleItems" :item="item" />
          </div>
       </div>
