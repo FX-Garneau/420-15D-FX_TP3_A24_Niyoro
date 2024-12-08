0efc94eaa6c065f486ae768be0fc149635c0e126
Ulric Huot <ulric.huot@gmail.com>
2024-12-08 04:37:23 -0500

    ✨ Update ItemCard component to enhance item title display with icons and adjust card style

--- site/src/components/ItemCard.vue
+++ site/src/components/ItemCard.vue
@@ -9,15 +9,19 @@ const tagsStore = useTagsStore()
 </script>
 
 <template>
-   <div class="card card-bordered border-neutral bg-base-100 w-96 shadow-xl">
+   <div class="card card-compact card-bordered border-neutral bg-base-100 w-96 shadow-xl">
       <!-- <figure>
          <img src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp" alt="Shoes" />
       </figure> -->
       <div class="card-body">
-
-         <h2 class="card-title">
-            <a v-if="item.url" :href="item.url">{{ item.title }}</a>
+         <h2 class="card-title flex items-center">
+            <a v-if="item.url" :href="item.url" class="text-info">{{ item.title }} <i class="bi bi-link-45deg"></i></a>
             <template v-else>{{ item.title }}</template>
+            <span class="flex-grow flex justify-end gap-2">
+               <i v-if="item.sticky" class="bi bi-pin-angle-fill text-warning"></i>
+               <i v-if="item.private" class="bi bi-lock-fill text-error"></i>
+               <i v-else class="bi bi-globe2 text-info"></i>
+            </span>
          </h2>
          <p>{{ item.content }}</p>
          <div class="card-actions justify-end">
